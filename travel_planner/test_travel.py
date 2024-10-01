d9cb8a08784c9e9a83ad416e1abf9d082853a248
bleicher <lenn.bleicher@ostfalia.de>
2024-10-01 06:49:03 +0200

    adding functionality, test cases and pytest ini file

--- travel_planner/test_travel.py
+++ travel_planner/test_travel.py
@@ -2,7 +2,27 @@
 Some Unit Test-basics in Python
 
 Author: L. Bleicher
-Last modified: 25.09.24
+Last modified: 30.09.24
 
 """
+import pytest
+
+from travel import Travelplan
+
+
+@pytest.fixture
+def plan():
+    return Travelplan()
+
+
+@pytest.mark.example
+def test_can_add_country(plan):
+    plan.add("france")
+    assert "france" in plan.get_reiseziele()
+
+
+def test_add_country_increases_size_of_plan(plan):
+    plan.add("france")
+    assert plan.size() == 1
+
 
--- travel_planner/travel.py
+++ travel_planner/travel.py
@@ -4,7 +4,22 @@ Main script to show testable functions
 Application for planning fitness programs
 
 Author: L. Bleicher
-Last modified: 25.09.24
+Last modified: 30.09.24
 
 """
 
+from typing import List
+
+
+class Travelplan:
+    def __init__(self) -> None:
+        self.destinations: List[str] = []
+
+    def add(self, destination: str):
+        self.destinations.append(destination)
+
+    def get_reiseziele(self) -> List[str]:
+        return self.destinations
+
+    def size(self) -> int:
+        return len(self.destinations)
