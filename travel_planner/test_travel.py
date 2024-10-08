2c8b9b0ad9ed2ce9f42067fb6dba3ec748097a4a
bleicher <lenn.bleicher@ostfalia.de>
2024-10-08 12:53:02 +0200

    Remove Method und Add Method angepasst, dazugehörige Tests ergänzt.

--- travel_planner/test_travel.py
+++ travel_planner/test_travel.py
@@ -15,14 +15,56 @@ def plan():
     return Travelplan()
 
 
-@pytest.mark.example
+@pytest.mark.add
 def test_can_add_country(plan):
     plan.add("france")
     assert "france" in plan.get_reiseziele()
 
 
+@pytest.mark.add
 def test_add_country_increases_size_of_plan(plan):
     plan.add("france")
     assert plan.size() == 1
 
 
+@pytest.mark.add
+def test_should_raise_error(plan):
+    for i in ["England", "Austria", "Germany", "France", "Switzerland"]:
+        plan.add(i)
+    with pytest.raises(OverflowError):
+        plan.add("france")
+
+
+@pytest.mark.add
+def test_should_raise_error_if_country_already_in_plan(plan):
+    plan.add("france")
+    with pytest.raises(ValueError):
+        plan.add("france")
+
+
+@pytest.mark.remove
+def test_can_remove_country(plan):
+    plan.add("france")
+    plan.remove("france")
+    assert "france" not in plan.get_reiseziele()
+
+
+@pytest.mark.remove
+def test_remove_country_decreases_size(plan):
+    plan.add("france")
+    plan.add("germany")
+    plan.remove("france")
+    assert plan.size() == 1
+
+
+@pytest.mark.remove
+def test_remove_nonexistent_country_raises_error(plan):
+    with pytest.raises(ValueError):
+        plan.remove("spain")
+
+
+"""
+Aufgabe:
+- Land entfernen
+- Land darf nur 1 Mal in der Liste enthalten sein
+"""
--- travel_planner/travel.py
+++ travel_planner/travel.py
@@ -1,7 +1,7 @@
 """
 Main script to show testable functions
 
-Application for planning fitness programs
+Application for planning trips
 
 Author: L. Bleicher
 Last modified: 30.09.24
@@ -14,9 +14,21 @@ from typing import List
 class Travelplan:
     def __init__(self) -> None:
         self.destinations: List[str] = []
+        self.max_size = 5
 
     def add(self, destination: str):
-        self.destinations.append(destination)
+        if self.size() >= self.max_size:
+            raise OverflowError("Cannot add more countries.")
+        if destination in self.destinations:
+            raise ValueError(f"{destination} is already in the travel plan.")
+        else:
+            self.destinations.append(destination)
+
+    def remove(self, destination: str):
+        if destination not in self.destinations:
+            raise ValueError(f"{destination} is not in the travel plan.")
+        else:
+            self.destinations.remove(destination)
 
     def get_reiseziele(self) -> List[str]:
         return self.destinations
